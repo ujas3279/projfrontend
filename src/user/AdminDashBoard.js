7dab124903a8f3b74ac7b823d718ca794923eea0
avanit <avanit@gmail.com>
2021-04-07 11:07:05 +0530

    initial push

--- src/user/AdminDashBoard.js
+++ src/user/AdminDashBoard.js
@@ -49,14 +49,14 @@ const AdminDashBoard = () => {
         <h4 className="card-header">Admin Information</h4>
         <ul className="list-group">
           <li className="list-group-item">
-            <span className="badge badge-success mr-2">Name:</span> {name}
+            <span className="badge alert-success mr-2">Name:</span> {name}
           </li>
           <li className="list-group-item">
-            <span className="badge badge-success mr-2">Email:</span> {email}
+            <span className="badge alert-success mr-2">Email:</span> {email}
           </li>
 
           <li className="list-group-item">
-            <span className="badge badge-denger">Admin Area</span>
+            <span className="badge alert-danger">Admin Area</span>
           </li>
         </ul>
       </div>
