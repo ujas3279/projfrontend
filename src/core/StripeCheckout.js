c11615a2d156018d93fa90a030d56e7f74592f22
ujas3279 <ujas6520@gmail.com>
2021-04-28 11:00:18 +0530

    commit

--- src/core/StripeCheckout.js
+++ src/core/StripeCheckout.js
@@ -94,7 +94,7 @@ let famount=0;
         })
         setReload(!reload);
         createOrder(userId, usertoken, orderData);
-        //SendEmail(maildata);
+        SendEmail(maildata);
         
       })
       .catch(error => console.log(error));
